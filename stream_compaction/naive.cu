be7f9ccb616606fc8b2c3a9879d122f45b5ab153
Logan Dooley <ldooley@seas.upenn.edu>
2026-09-07 11:53:12 -0400

    Start naive scan setup

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -18,8 +18,45 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+            // Allocate buffers
+            int* dev_idata;
+            int* dev_odata;
+            cudaMalloc((void**)&dev_idata, n * sizeof(int));
+            cudaMalloc((void**)&dev_odata, n * sizeof(int));
+
+            // Copy input to CPU
+            cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+            scan_gpu(n, dev_odata, dev_idata);
+
+            // Copy output to CPU
+            cudaMemcpy(odata, dev_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+            // Free buffers
+            cudaFree(dev_idata);
+            cudaFree(dev_odata);
             timer().endGpuTimer();
+        }
+
+        __host__ __device__ int divup(int dividend, int divisor) {
+            return (dividend + divisor - 1) / divisor;
+        }
+
+        __host__ void pick_block_size(int n, int* numBlocks, int* blockSize) {
+            *blockSize = 1024;
+            *numBlocks = divup(n, *blockSize);
+        }
+
+        __global__ void kernScan(int n, int* dev_odata, int* dev_idata) {
+
+        }
+
+        void scan_gpu(int n, int* dev_odata, const int* dev_idata) {
+            int numBlocks = 0;
+            int blockSize = 0;
+            pick_block_size(n, &numBlocks, &blockSize);
+
+
         }
     }
 }
--- stream_compaction/naive.h
+++ stream_compaction/naive.h
@@ -7,5 +7,7 @@ namespace StreamCompaction {
         StreamCompaction::Common::PerformanceTimer& timer();
 
         void scan(int n, int *odata, const int *idata);
+
+        void scan_gpu(int n, int* dev_odata, const int* dev_idata);
     }
 }
